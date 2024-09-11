4ac954ad8a2cda408564c1770e42e2b1692648d4
Philip Bordallo <git@philipbordallo.com>
2024-09-11 00:59:10 -0400

    Add coscan package (#6)

---
+++ packages/coscan/rollup.config.js
@@ -0,0 +1,36 @@
+import pluginBabel from '@rollup/plugin-babel';
+import pluginNodeResolve from '@rollup/plugin-node-resolve';
+import path from 'node:path';
+import { defineConfig } from 'rollup';
+import pkg from './package.json' with { type: 'json' };
+
+const esmDirectory = path.dirname(pkg.exports['.'].import);
+const cjsDirectory = path.dirname(pkg.exports['.'].require);
+
+export default defineConfig({
+  input: pkg.main,
+  output: [
+    {
+      dir: esmDirectory,
+      format: 'esm',
+      preserveModules: true,
+      preserveModulesRoot: './src',
+    },
+    {
+      dir: cjsDirectory,
+      format: 'cjs',
+      preserveModules: true,
+      preserveModulesRoot: './src',
+    },
+  ],
+  external: [
+    /node_modules/,
+  ],
+  plugins: [
+    pluginNodeResolve(),
+    pluginBabel({
+      babelHelpers: 'runtime',
+      extensions: ['.ts'],
+    }),
+  ],
+});
