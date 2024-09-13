fec1495ff9c6aac52ef19f6be50961f2df69cfb3
Philip Bordallo <git@philipbordallo.com>
2024-09-13 15:06:25 +0000

    Organize devtools

---
+++ devtools/rollup/src/rollup.ts
@@ -0,0 +1,56 @@
+import pluginBabel from '@rollup/plugin-babel';
+import pluginNodeResolve from '@rollup/plugin-node-resolve';
+import { defineConfig, type OutputOptions } from 'rollup';
+
+type PackageExports = Record<string, { import: string; require: string; types: string }> | string;
+
+function getOutput(pkgExports: PackageExports): OutputOptions[] {
+  if (typeof pkgExports === 'string') {
+    return [
+      {
+        file: pkgExports,
+        format: 'esm',
+      },
+    ];
+  }
+
+  if (pkgExports['.']) {
+    return [
+      {
+        file: pkgExports['.'].import,
+        format: 'esm',
+      },
+      {
+        file: pkgExports['.'].require,
+        format: 'cjs',
+      },
+    ];
+  }
+
+  throw new Error('Invalid package `exports` field.');
+}
+
+type Package = {
+  main: string;
+  exports: PackageExports;
+};
+
+export function createRollupConfig(pkg: Package) {
+  const output = getOutput(pkg.exports);
+
+  return defineConfig({
+    input: pkg.main,
+    output,
+    external: [
+      /@coscan/,
+      /node_modules/,
+    ],
+    plugins: [
+      pluginNodeResolve(),
+      pluginBabel({
+        babelHelpers: 'runtime',
+        extensions: ['.ts'],
+      }),
+    ],
+  });
+}
--- packages/jsx-scanner/rollup.config.js
+++ packages/jsx-scanner/rollup.config.js
@@ -1,4 +1,4 @@
-import { createRollupConfig } from '@coscan/devtools';
+import { createRollupConfig } from '@coscan/rollup';
 import pkg from './package.json' with { type: 'json' };
 
 export default createRollupConfig(pkg);
