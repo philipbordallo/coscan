e61312db811ad63862f7f4c8644589be91867b2f
Philip Bordallo <git@philipbordallo.com>
2024-09-16 05:05:17 +0000

    Update how prop expressions are parsed

--- packages/jsx-scanner/src/entities/prop.ts
+++ packages/jsx-scanner/src/entities/prop.ts
@@ -2,6 +2,8 @@ import {
   isIdentifier,
   isJsxAttribute,
   isJsxExpression,
+  isObjectLiteralExpression,
+  isPropertyAssignment,
   isStringLiteral,
   type JsxAttributeName,
   type JsxAttributes,
@@ -11,10 +13,14 @@ import {
 import { isBooleanLiteral } from '../guards/boolean-literal.ts';
 
 export type Prop = string;
-export type PropValue = string | boolean | undefined;
+export type PropValue = string | boolean | Record<string, string> | undefined;
 export type Props = {
   [prop: Prop]: PropValue;
 };
+
+function formatExpression(value: string): string {
+  return `Expression -> ${value}`;
+}
 
 function getPropValue(value?: JsxAttributeValue, sourceFile?: SourceFile): PropValue {
   // If a value is not defined, it means the prop is a boolean
@@ -35,8 +41,29 @@ function getPropValue(value?: JsxAttributeValue, sourceFile?: SourceFile): PropV
     }
 
     if (isIdentifier(value.expression)) {
-      return `Expression: {${expression}}`;
+      return formatExpression(expression);
     }
+
+    if (isObjectLiteralExpression(value.expression)) {
+      const entries = value.expression.properties
+        .map((prop) => {
+          if (isPropertyAssignment(prop)) {
+            const key = prop.name.getText(sourceFile);
+            const initializer = prop.initializer.getText(sourceFile);
+
+            const value = isStringLiteral(prop.initializer)
+              ? initializer.replace(/['"]+/g, '')
+              : formatExpression(initializer);
+
+            return [key, value];
+          }
+        })
+        .filter((entry) => entry !== undefined);
+
+      return Object.fromEntries(entries);
+    }
+
+    return expression;
   }
 }
 
