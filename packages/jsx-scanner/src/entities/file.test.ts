82bb34351ec5d5885c673fa4653c1bdebdf73811
Philip Bordallo <git@philipbordallo.com>
2024-09-28 16:24:42 -0700

    Use path to resolve file paths (#31)

--- packages/jsx-scanner/src/entities/file.test.ts
+++ packages/jsx-scanner/src/entities/file.test.ts
@@ -1,6 +1,8 @@
 import { afterAll, beforeAll, describe, expect, it, jest } from '@jest/globals';
-import { createSourceFile } from 'typescript';
+import { createSourceFile, ScriptTarget } from 'typescript';
 import { getRelativeFilePath } from './file.ts';
+
+const DEFAULT_OPTIONS = ['export const hello = "world"', ScriptTarget.ESNext] as const;
 
 describe(getRelativeFilePath, () => {
   beforeAll(() => {
@@ -11,15 +13,21 @@ describe(getRelativeFilePath, () => {
     jest.restoreAllMocks();
   });
 
-  it('returns the relative file path', () => {
-    const sourceFile = createSourceFile('/path/to/project/src/file.ts', 'export const hello = "world"', 1);
+  it('returns the file path if it already is a relative path', () => {
+    const sourceFile = createSourceFile('src/file.ts', ...DEFAULT_OPTIONS);
 
-    expect(getRelativeFilePath(sourceFile)).toBe('src/file.ts');
+    expect(getRelativeFilePath(sourceFile.fileName)).toBe('src/file.ts');
   });
 
-  it('returns the file path if it already is a relative path', () => {
-    const sourceFile = createSourceFile('src/file.ts', 'export const hello = "world"', 1);
+  it('returns the relative file path if it matches the current working directory', () => {
+    const sourceFile = createSourceFile('/path/to/project/src/file.ts', ...DEFAULT_OPTIONS);
 
-    expect(getRelativeFilePath(sourceFile)).toBe('src/file.ts');
+    expect(getRelativeFilePath(sourceFile.fileName)).toBe('src/file.ts');
+  });
+
+  it('returns an absolute path if the current working directory does not match the path', () => {
+    const sourceFile = createSourceFile('/different/path/src/file.ts', ...DEFAULT_OPTIONS);
+
+    expect(getRelativeFilePath(sourceFile.fileName)).toBe('/different/path/src/file.ts');
   });
 });
--- packages/jsx-scanner/src/entities/file.ts
+++ packages/jsx-scanner/src/entities/file.ts
@@ -1,13 +1,14 @@
-import type { SourceFile } from 'typescript';
+import path from 'node:path';
 
 export type FilePath = string;
+export type Directory = string;
 
-export function getRelativeFilePath(sourceFile: SourceFile): FilePath {
+export function getRelativeFilePath(filePath: FilePath): FilePath {
   const currentWorkingDirectory = process.cwd();
 
-  const relativeFilePath = sourceFile.fileName.startsWith(currentWorkingDirectory)
-    ? sourceFile.fileName.replace(currentWorkingDirectory + '/', '')
-    : sourceFile.fileName;
+  if (filePath.startsWith(currentWorkingDirectory)) {
+    return path.relative(currentWorkingDirectory, filePath);
+  }
 
-  return relativeFilePath;
+  return filePath;
 }
