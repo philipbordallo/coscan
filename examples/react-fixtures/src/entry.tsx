dca9d28b6ce7ef0cf4b6bf7783f276822ed09643
Philip Bordallo <git@philipbordallo.com>
2024-10-14 00:43:56 +0000

    Add entry and scan to react-fixtures example

---
+++ examples/react-fixtures/src/entry.tsx
@@ -0,0 +1,24 @@
+import { ButtonComponent as Button } from './components/button-component.tsx';
+import ClassComponent from './components/class-components.tsx';
+import { UnnamedFragmentComponent } from './components/fragment-components.tsx';
+import { FunctionArrowComponent } from './components/function-components.tsx';
+import { ImportComponent } from './components/import-component.tsx';
+import { add } from './components/no-component.tsx';
+import { StylePropComponent } from './components/prop-component.tsx';
+import { ReturnTypeReactElement } from './components/return-type-components.tsx';
+import { SubComponent } from './components/sub-components.tsx';
+
+export function Entry() {
+  return (
+    <>
+      <Button>{add(1, 2)}</Button>
+      <ClassComponent />
+      <UnnamedFragmentComponent />
+      <FunctionArrowComponent />
+      <ImportComponent />
+      <StylePropComponent />
+      <ReturnTypeReactElement />
+      <SubComponent />
+    </>
+  );
+}
