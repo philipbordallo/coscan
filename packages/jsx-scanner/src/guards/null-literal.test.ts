211c2517a3f1ddb77ac764b7adaa90d3abc01c4e
Philip Bordallo <git@philipbordallo.com>
2024-11-12 07:58:21 -0500

    Add test utilities (#58)

--- packages/jsx-scanner/src/guards/null-literal.test.ts
+++ packages/jsx-scanner/src/guards/null-literal.test.ts
@@ -1,17 +1,24 @@
 import { describe, expect, it } from '@jest/globals';
-import { factory } from 'typescript';
+import { SyntaxKind } from 'typescript';
+import { createTestSourceFile, queryNodeKind } from '../entities/test-utilities.ts';
 import { isNullLiteral } from './null-literal.ts';
 
 describe(isNullLiteral, () => {
   it('returns true for null literal', () => {
-    const nullLiteral = factory.createNull();
+    const content = 'const test = null;';
+
+    const sourceFile = createTestSourceFile({ content });
+    const nullLiteral = queryNodeKind(SyntaxKind.NullKeyword, sourceFile);
 
     expect(isNullLiteral(nullLiteral)).toBe(true);
   });
 
   it('returns false for other nodes', () => {
-    const stringLiteral = factory.createStringLiteral('');
-    const numericLiteral = factory.createNumericLiteral('0');
+    const content = 'const options = ["hello", 1];';
+
+    const sourceFile = createTestSourceFile({ content });
+    const stringLiteral = queryNodeKind(SyntaxKind.StringLiteral, sourceFile);
+    const numericLiteral = queryNodeKind(SyntaxKind.NumericLiteral, sourceFile);
 
     expect(isNullLiteral(stringLiteral)).toBe(false);
     expect(isNullLiteral(numericLiteral)).toBe(false);
