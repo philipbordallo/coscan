dde816e683757bb3854acc61402c81b91ab73332
Philip Bordallo <git@philipbordallo.com>
2024-11-12 12:57:11 +0000

    Remove additional uses of factory for tests

--- packages/jsx-scanner/src/guards/boolean-literal.test.ts
+++ packages/jsx-scanner/src/guards/boolean-literal.test.ts
@@ -1,20 +1,33 @@
 import { describe, expect, it } from '@jest/globals';
-import { factory } from 'typescript';
+import { SyntaxKind } from 'typescript';
+import { createTestSourceFile, queryNodeKind } from '../entities/test-utilities.ts';
 import { isBooleanLiteral } from './boolean-literal.ts';
 
 describe(isBooleanLiteral, () => {
   it('returns true for true literal', () => {
-    const trueLiteral = factory.createTrue();
+    const content = 'const test = true;';
+
+    const sourceFile = createTestSourceFile({ content });
+    const trueLiteral = queryNodeKind(SyntaxKind.TrueKeyword, sourceFile);
+
     expect(isBooleanLiteral(trueLiteral)).toBe(true);
   });
 
   it('returns true for false literal', () => {
-    const falseLiteral = factory.createFalse();
+    const content = 'const test = false;';
+
+    const sourceFile = createTestSourceFile({ content });
+    const falseLiteral = queryNodeKind(SyntaxKind.FalseKeyword, sourceFile);
+
     expect(isBooleanLiteral(falseLiteral)).toBe(true);
   });
 
   it('returns false for other nodes', () => {
-    const nullLiteral = factory.createNull();
+    const content = 'const test = null;';
+
+    const sourceFile = createTestSourceFile({ content });
+    const nullLiteral = queryNodeKind(SyntaxKind.NullKeyword, sourceFile);
+
     expect(isBooleanLiteral(nullLiteral)).toBe(false);
   });
 });
